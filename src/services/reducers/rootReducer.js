8e4d19764cee6287653b0fd3f1d220ea2b3b1cdc
Evgenia Kurakina <krokodila888@yandex.ru>
2023-06-16 01:37:18 +0300

    next test

--- src/services/reducers/rootReducer.js
+++ src/services/reducers/rootReducer.js
@@ -1,13 +1,6 @@
 import { combineReducers } from 'redux';
 import { cardsReducer } from "./cardsReducer";
-import {locationReducer} from './locationReducer';
 
 export const rootReducer = combineReducers({
-  //ingredientsReducer,
-  cardsReducer,
-  /*currentIngredientReducer,
-  currentOrderReducer,
-  resetPasswordReducer,
-  authReducer,*/
-  locationReducer
+  cardsReducer
 }) 

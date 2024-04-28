fc3382f9b8da6b5c13a791987a8cb528c55d7b91
krokodila888 <krokodila888@yandex.ru>
2024-04-28 20:19:08 +0300

    FIX: added linters

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'stylelint-config-standard-scss',
+  rules: {
+    'selector-class-pattern': null,
+    'selector-type-case': null,
+    'selector-type-no-unknown': null,
+    'property-no-unknown': null,
+  },
+};
